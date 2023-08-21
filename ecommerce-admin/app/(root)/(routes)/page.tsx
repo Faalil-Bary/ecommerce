dfee8e7f2b761d637358db5d28d4257e79a53a93
faalil-bary <muhammadhfaalil@gmail.com>
2023-08-22 00:48:26 +0530

    feat : Dashboard Initilization

--- ecommerce-admin/app/(root)/(routes)/page.tsx
+++ ecommerce-admin/app/(root)/(routes)/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
+import { useParams } from "next/navigation";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 
-
-const SetupPage =() => {
+const SetupPage = () => {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
 
@@ -15,11 +15,7 @@ const SetupPage =() => {
     }
   }, [isOpen, onOpen]);
 
-    return (
-        <div className="p-4">
-           Root Page
-        </div>
-    );
-}
-
-export default SetupPage;
+  return null;
+};
+ 
+export default SetupPage
